c3166d596d414eec3dc0989328524bc533ca8a79
mhtran3 <mhtran@princeton.edu>
2022-12-13 16:13:04 -0500

    Base files for enemy mob

---
+++ src/components/objects/Enemy/Enemy.js
@@ -0,0 +1,24 @@
+import { Group } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import MODEL from './paperMob.gltf';
+
+class Enemy extends Group {
+    constructor() {
+        // Call parent Group() constructor
+        super();
+
+        
+        this.name = 'enemy';
+    }
+
+    addEnemy() {
+        
+        // Load object
+        const loader = new GLTFLoader();
+        loader.load(MODEL, (gltf) => {
+            this.add(gltf.scene);
+        });
+    }
+}
+
+export default Enemy;
