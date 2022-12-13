a3d7fd4e620b56e2137d24e7f53ea3edce27ca6a
nadiarodriguez00 <78522724+nadiarodriguez00@users.noreply.github.com>
2022-12-12 23:40:06 -0500

    starter code

---
+++ src/components/objects/Flower/Flower.js
@@ -0,0 +1,49 @@
+import { Group } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js';
+import MODEL from './flower.gltf';
+
+class Flower extends Group {
+    constructor(parent) {
+        // Call parent Group() constructor
+        super();
+
+        // Init state
+        this.state = {
+            gui: parent.state.gui,
+            bob: true,
+            twirl: 0,
+        };
+
+        // Load object
+        const loader = new GLTFLoader();
+
+        this.name = 'flower';
+        loader.load(MODEL, (gltf) => {
+            this.add(gltf.scene);
+        });
+
+        // Add self to parent's update list
+        parent.addToUpdateList(this);
+
+        // Populate GUI
+        this.state.gui.add(this.state, 'bob');
+    }
+
+    update(timeStamp) {
+        if (this.state.bob) {
+            // Bob back and forth
+            this.rotation.z = 0.05 * Math.sin(timeStamp / 300);
+        }
+        if (this.state.twirl > 0) {
+            // Lazy implementation of twirl
+            this.state.twirl -= Math.PI / 8;
+            this.rotation.y += Math.PI / 8;
+        }
+
+        // Advance tween animations, if any exist
+        TWEEN.update();
+    }
+}
+
+export default Flower;
