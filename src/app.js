767ec44f22f8b1268ecb513c5e917dc1e4e232c1
Dylan Tran <ddtran@princeton.edu>
2022-12-15 02:15:48 -0500

    increased max camera distance

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.enablePan = false;
 controls.minDistance = 4;
-controls.maxDistance = 16;
+controls.maxDistance = 40;
 controls.update();
 
 /////////////
